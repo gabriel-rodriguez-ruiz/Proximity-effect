c9db399d1e9162f097a2e66e213548fcab180653
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2024-10-14 10:07:01 -0300

    First results with cluster.

--- plot_superconducting_density.py
+++ plot_superconducting_density.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 data_folder = Path("Data/")
-name = "n_By_mu_-39.9_L=600_h=0.01_B_y_in_(0.0-1.0)_Delta=2_lambda=0.56_w_s=10_w_S=20_w_1=0.5_points=16.npz"
+name = "n_By_mu_-34.4_L=600_h=0.01_B_y_in_(0.0-1.0)_Delta=2_lambda=0.56_w_s=10_w_S...=False.npz"
 file_to_open = data_folder / name
 
 data = np.load(file_to_open)
